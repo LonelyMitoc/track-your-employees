025cc6d0e71dc4a532f6ba77f1fa4946f83dcd06
Triglyceride <jamesmatsu@gmail.com>
2022-11-08 16:49:36 -0800

    added more to app.js

--- app.js
+++ app.js
@@ -1,4 +1,27 @@
 const mysql = require('mysql2');
 const inquirer = requre('inquirer');
 const consoleTable = require('console.table');
+const connection = require('./config/connection');
 
+const welcomeMsg = `====== WELCOME TO THE EMPLOYEE MANAGER =======`;
+
+const welcome = () => {
+    return inquirer
+        .prompt ([
+            {
+                type: 'input',
+                message: welcomeMsg + `\nHere you have access to viewing, adding, editing employees, roles and departments information.\nHit ENTER to continue.\n`,
+                name: 'welcome',
+            },
+        ])
+        .then(startPrompt)
+};
+
+const startPrompt = async () => {
+    return await inquirer
+        .prompt([
+            {
+                
+            }
+        ])
+}

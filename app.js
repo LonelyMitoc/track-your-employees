841f7d2eb6adbd91b148d44e0479f33976df16a4
Triglyceride <jamesmatsu@gmail.com>
2022-11-09 16:01:11 -0800

    added add employee script

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ connection.connect((err) => {
     welcome();
 });
 
+//Welcome message when the program is first initialized
 const welcome = () => {
     return inquirer
         .prompt ([
@@ -22,6 +23,7 @@ const welcome = () => {
         .then(mainMenu())
 };
 
+// The main menu that user's will come back to
 async function mainMenu() {
     const response = await inquirer
         .prompt([
@@ -68,7 +70,8 @@ async function mainMenu() {
     }
 }
 
-function viewEmployee() {
+// View employee function
+async function viewEmployee() {
     connection.query(
         `SELECT employees.first_name AS First_Name, employees.last_name AS Last_Name, roles.title AS Role, departments.department_name AS Department, roles.salary AS Salary, CONCAT(managers.first_name, " ", managers.last_name) AS Manager FROM employees
             LEFT JOIN roles ON employees.role_id = roles.id
@@ -82,4 +85,95 @@ function viewEmployee() {
         }
     );
 };
+
+//Add employee function
+async function addEmployee() {
+    connection.query(
+        `SELECT * FROM departments ORDER BY id`,
+        async function(err, availDep) {
+            if (err) throw err;
+            const dep = availDep.map((data) => data.department_name);
+            const resDep = await inquirer.prompt({
+                type: 'list',
+                message: 'Which department does the new employee work in?',
+                choices: dep,
+                name: 'department',
+            });
+
+            let depIndex = departments.indexOf(resDep.department);
+            connection.query(
+                `SELECT * FROM roles WHERE department_id = ${availDep[depIndex].id} ORDER BY id`,
+                async function (err2, availRoles) {
+                    if (err2) throw err2;
+                    const roles = availRoles.map((data) => data.title);
+                    const resRole = await inquirer.prompt({
+                        type: 'list',
+                        message: 'Select the role with in the department for the new employee:',
+                        choices: roles,
+                        name: 'role',
+                    });
+                    let roleIndex = null;
+                    for (let i = 0; i < availRoles.length; i++) {
+                        if (availRoles[i].title === resRole.role) {
+                            roleIndex = i;
+                            break;
+                        }
+                    }
+                    connection.query(
+                        `SELECT CONCAT(first_name, " ", last_name) as name, id FROM employees ORDER BY id;`,
+                        async function (err3, availMng) {
+                            if (err3) throw err3;
+                            const managers = ['none', ...availMng.map((data) => data.name)];
+                            const res = await inquirer.prompt([
+                                {
+                                    type: 'list',
+                                    message: 'Who will the new employee report to?',
+                                    choices: managers,
+                                    name: 'manager',
+                                },
+                                {
+                                    type: 'input',
+                                    message: 'Enter the first name of the new employee:',
+                                    name: 'first_name',
+                                },
+                                {
+                                    type: 'input',
+                                    message: 'Enter the last name of the new employee:',
+                                    name: 'last_name',
+                                },
+                            ]);
+
+                            let mngIndex = null;
+                            for (let i; i < availMng.length; i++) {
+                                if ((availMng[i].name = response.manager)) {
+                                    mngIndex = i;
+                                }
+                            }
+
+                            let insertText = `INSERT INTO employees(first_name, last_name, role_id`;
+                            if (mngIndex === null) {
+                                insertText += `)`;
+                            } else {
+                                insertText += `, manager_id)`;
+                            }
+
+                            let empValues = `VALUES ('${res.first_name}', '${res.last_name}', '${availRoles[roleIndex].id}'`;
+                            if (mngIndex === null) {
+                                empValues += `)`;
+                            } else {
+                                empValues += `, '${availMng[mngIndex].id}')`;
+                            }
+                            const insert = insertText + ` ` + empValues;
+                            connection.query(insert, function (err4, response) {
+                                if (err4) throw err4;
+                                console.log(`${res.first_name} ${res.last_name} added successfully to EMPLOYEE.`);
+                                viewEmployee();
+                            });
+                        }
+                    );
+                }
+            );
+        }
+    );
+}
 
